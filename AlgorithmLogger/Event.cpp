6d34b7bce1598b42e23882f91ff12854b91b918f
steve <551619855@qq.com>
2018-01-14 12:00:43 +0800

    Load data from csv file.(Finished but it is extremely slow).

--- AlgorithmLogger/Event.cpp
+++ AlgorithmLogger/Event.cpp
@@ -5,6 +5,9 @@
 #include "Event.h"
 
 namespace AWF{
-
+ Event::Event(std::string name) {
+     event_name_ = name;
+     event_time_stamp_ = std::chrono::system_clock::now();
+ }
 
 }
--- AlgorithmLogger/Event.h
+++ AlgorithmLogger/Event.h
@@ -8,18 +8,19 @@
 
 #include <iostream>
 
-namespace AWF{
- class Event {
- public:
-     Event(std::string name);
+#include <chrono>
 
- protected:
-     std::string event_name_ = "null";
-     double event_time_stamp_ = 0.0;
+namespace AWF {
+    class Event {
+    public:
+        Event(std::string name);
+
+    protected:
+        std::string event_name_ = "null";
+        double event_time_stamp_ = 0.0;
 
 
-
-};
+    };
 
 }
 
