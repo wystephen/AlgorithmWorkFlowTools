c6d5f7813c99aa88ccb6900779bafa32a5444d41
steve <551619855@qq.com>
2018-01-18 00:08:23 +0800

    singleton algorithmLogger.

--- AlgorithmLogger/Event.h
+++ AlgorithmLogger/Event.h
@@ -11,8 +11,8 @@
 #include <chrono>
 
 
-
 namespace AWF {
+    template <typename T>
     class Event {
     public:
         Event(std::string name) :
@@ -20,24 +20,48 @@ namespace AWF {
                 event_time_stamp_(
                         double(std::chrono::duration_cast<std::chrono::microseconds>(
                                 std::chrono::system_clock::now().time_since_epoch()
-                        ).count()) / 1000000.0)
-        {
+                        ).count()) / 1000000.0) {
 
 
         }
 
-        virtual std::string toString(){
-            return std::string(std::to_string(event_time_stamp_));
+        /**
+         * convert the event to string.
+         * @return
+         */
+        virtual std::string toString() {
+            return "["+ std::string(std::to_string(event_time_stamp_))+"]:"+event_name_;
+        }
+
+        /**
+         * set data based on type name.
+         * @param d
+         * @return
+         */
+        bool setData(T d){
+            data_ = d;
+            return true;
         }
 
     protected:
         std::string event_name_ = "null";
         double event_time_stamp_ = 0.0;
-
+        T data_;
 
 
     };
 
+    class StringEvent : public Event<std::string> {
+    public:
+        StringEvent::StringEvent(const std::string &name) : Event("stringEvent_" + name) {}
+
+
+        virtual std::string toString() {
+            return ((Event*)this)->toString()+":"+data_;
+        }
+
+
+    };
 
 
 }
