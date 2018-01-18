a0a43f60fbd81606fe97736ce66e57a7a9ebdf2b
steve <551619855@qq.com>
2018-01-19 00:47:45 +0800

    algorithm workflow.

---
+++ AWF.h
@@ -0,0 +1,13 @@
+//
+// Created by steve on 18-1-19.
+//
+
+#ifndef ALGORITHMWORKFLOWTOOLS_AWF_H
+#define ALGORITHMWORKFLOWTOOLS_AWF_H
+
+#include "ExtentedIO/FileReader.h"
+#include "ExtentedIO/FileReader.cpp"
+
+
+
+#endif //ALGORITHMWORKFLOWTOOLS_AWF_H
