8e915539ae67e931bb8fec4b6fe11d1c251e803f
steve <551619855@qq.com>
2018-01-19 23:22:11 +0800

    algorithm workflow.

--- Test/AlgorithmLoggerTester.cpp
+++ Test/AlgorithmLoggerTester.cpp
@@ -21,19 +21,34 @@ int main() {
 
     AWF::AlgorithmLogger *logger_ptr = AWF::AlgorithmLogger::getInstance();
 
-    for (int i = 0; i < 1; i++) {
-        std::thread t([] {
-//            std::cout << std::time_put(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))
+    for (int i = 0; i < 100; i++) {
+        try {
+            std::thread t([] {
+                try {
+                    std::cout << double(
+                            std::chrono::duration_cast<std::chrono::milliseconds>(
+                                    std::chrono::system_clock::now().time_since_epoch()
+                            ).count()
+                    ) << std::endl;
 
 
-            for (int j = 0; j < 1000; ++j) {
-                AWF::AlgorithmLogger *logger_ptr = AWF::AlgorithmLogger::getInstance();
-                std::cout << logger_ptr->getName() << std::endl;
+                    for (int j = 0; j < 1000; ++j) {
+//                    AWF::AlgorithmLogger *logger_ptr = AWF::AlgorithmLogger::getInstance();
+//                    std::cout << logger_ptr->getName() << std::endl;
 //                AWF::StringEvent e(std::to_string(j));
-            }
+                    }
+                } catch (std::exception &e) {
+                    std::cout << e.what() << std::endl;
+                }
 
-        });
-//        t.detach();
+
+            });
+//            t.detach();
+//            t.join();
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+
     }
     int k(0);
     std::cin >> k;
