27ad5aae6c036ef2c2c184e3e9f58904e6741bc7
steve <551619855@qq.com>
2018-03-29 19:43:40 +0800

    Only Draw Function.--- matplotlib version. With a problem Cannot use.

--- UsefulTools/DataOnlineDisplay.h
+++ UsefulTools/DataOnlineDisplay.h
@@ -27,9 +27,47 @@
 #ifndef ALGORITHMWORKFLOWTOOLS_DATAONLINEDISPLAY_H
 #define ALGORITHMWORKFLOWTOOLS_DATAONLINEDISPLAY_H
 
+
+#include <iostream>
+#include <thread>
+#include <Eigen/Dense>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+
+#include <vtkVersion.h>
+#include <vtkSmartPointer.h>
+
+#include <vtkChartXY.h>
+#include <vtkContextScene.h>
+#include <vtkContextView.h>
+#include <vtkFloatArray.h>
+#include <vtkPlotPoints.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkTable.h>
+#include <vtkAutoInit.h>
+#include <vtkVariantArray.h>
+
 namespace AWF {
     class DataOnlineDisplay {
 
+
+
+    private:
+        std::string win_name_;
+        int dim_;
+
+
+        std::mutex buffer_mutex_;
+
+        std::vector<Eigen::VectorXd> input_buffer_ = std::vector<Eigen::VectorXd>();
+        std::thread draw_thread_;
+
+        std::atomic<bool> run_flag_;
+
+        double sleep_time_ = 0.5;
     };
 }
 
