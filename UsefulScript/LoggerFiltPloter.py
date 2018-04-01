066707706c8bf847cfa952fe52858f4fa907dbc9
steve <551619855@qq.com>
2018-04-01 14:52:44 +0800

    algorithm out put is ok.

---
+++ UsefulScript/LoggerFiltPloter.py
@@ -0,0 +1,53 @@
+# Created by steve on 18-4-1 下午2:30
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+import scipy as sp
+
+from pandas import DataFrame
+
+import re
+import os
+
+
+class LoggerFilePlotting:
+    def __init__(self, file_name):
+        self.log_file = open(file_name)
+
+        all_lines = self.log_file.readlines()
+
+        self.num_re = re.compile('[-]{0,1}[0-9]')
+
+        for line in all_lines:
+            # print(line)
+            time_str = line.split(':')[0]
+            cate_str = line.split(':')[1]
+            vec_str = line.split(':')[2]
+            print(time_str, cate_str, vec_str)
+
+
+if __name__ == '__main__':
+    lfp = LoggerFilePlotting("logger_2018-03-31-17:20:37.log")
