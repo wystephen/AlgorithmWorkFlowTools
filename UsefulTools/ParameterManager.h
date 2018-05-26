149db7760b37f07982e71914b902855cb9ce1734
steve <551619855@qq.com>
2018-05-26 18:52:51 +0800

    corrected regex in LoggerFiltPloter.py ...

---
+++ UsefulTools/ParameterManager.h
@@ -0,0 +1,21 @@
+//
+// Created by steve on 18-5-26.
+//
+
+#ifndef ALGORITHMWORKFLOWTOOLS_PARAMETERMANAGER_H
+#define ALGORITHMWORKFLOWTOOLS_PARAMETERMANAGER_H
+
+#include <iostream>
+
+#include <fstream>
+
+class ParameterManager {
+
+
+public:
+	ParameterManager
+
+};
+
+
+#endif //ALGORITHMWORKFLOWTOOLS_PARAMETERMANAGER_H
